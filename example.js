ffdd402b24faed8853390a58790abd9397c31622
Irakli Nadareishvili <irakli@gmail.com>
2013-07-20 14:06:26 +0000

    util, log and npmlog all implemented! w00t

--- example.js
+++ example.js
@@ -1,6 +1,14 @@
 var log = require('./lib/metalogger')('npmlog', 'debug');
-
 var util = require('util');
+
+var obj = {
+  "name"         : "Irakli"
+, "lastname"     : "Nadareishvili"
+, "organization" : "NPR" 
+, "languages"    : ["Node.js", "Java", "PHP", "Python", "Ruby", "TCL"]
+, "tvshows"      : ["Burn Notice", "Top Gear", "Scrubs"]
+}
+//------ Npmlog.js
 
 log.info('something info');
 log.error('something error');
@@ -10,8 +18,13 @@ log.warning('something warning');
 log.alert('something alert');
 log.critical('something critical');
 
+log.info(obj);
+
 // Complex syntax:
 log.error('caption', 'num: %d, title: %s', 125, "npr");
+
+
+//------ Log.js
 
 log = require('./lib/metalogger')('log', 'debug');
 
@@ -23,8 +36,24 @@ log.warning('something warning');
 log.alert('something alert');
 log.critical('something critical');
 
-// args = ['num: %d, title: %s', 125, "npr"]
-// console.log(util.format.apply(null, args));
+log.info(obj);
+
+// Complex syntax:
+log.error('caption', 'num: %d, title: %s', 125, "npr");
+
+//------ UTIL
+
+log = require('./lib/metalogger')('util', 'debug');
+
+log.info('something info');
+log.error('something error');
+log.debug('something debug');
+log.notice('something notice');
+log.warning('something warning');
+log.alert('something alert');
+log.critical('something critical');
+
+log.info(obj);
 
 // Complex syntax:
 log.error('caption', 'num: %d, title: %s', 125, "npr");
