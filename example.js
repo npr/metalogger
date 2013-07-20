bc60e90c10a4497979e3e73dccff86e9761d7a1d
Irakli Nadareishvili <irakli@gmail.com>
2013-07-20 13:28:03 +0000

    almost done implementing log.js

--- example.js
+++ example.js
@@ -1,5 +1,7 @@
 var log = require('./lib/metalogger')('npmlog', 'debug');
 
+var util = require('util');
+
 log.info('something info');
 log.error('something error');
 log.debug('something debug');
@@ -11,8 +13,7 @@ log.critical('something critical');
 // Complex syntax:
 log.error('caption', 'num: %d, title: %s', 125, "npr");
 
-
-log = require('./lib/metalogger')('log', 'info');
+log = require('./lib/metalogger')('log', 'debug');
 
 log.info('something info');
 log.error('something error');
@@ -21,3 +22,9 @@ log.notice('something notice');
 log.warning('something warning');
 log.alert('something alert');
 log.critical('something critical');
+
+// args = ['num: %d, title: %s', 125, "npr"]
+// console.log(util.format.apply(null, args));
+
+// Complex syntax:
+log.error('caption', 'num: %d, title: %s', 125, "npr");
