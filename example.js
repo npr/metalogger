4a1c7342bbc5a6b91ba10534744b0bdf9b0b29b1
Irakli Nadareishvili <irakli@gmail.com>
2013-07-20 12:50:43 +0000

    npmlog successfully implemented

--- example.js
+++ example.js
@@ -1,9 +1,23 @@
-var log = require('./lib/metalogger')('npmlog', 'trace');
-
-//console.dir(log);
+var log = require('./lib/metalogger')('npmlog', 'debug');
 
 log.info('something info');
 log.error('something error');
 log.debug('something debug');
-log.trace('something trace');
-log.warn('kkk');
+log.notice('something notice');
+log.warning('something warning');
+log.alert('something alert');
+log.critical('something critical');
+
+// Complex syntax:
+log.error('caption', 'num: %d, title: %s', 125, "npr");
+
+
+log = require('./lib/metalogger')('log', 'info');
+
+log.info('something info');
+log.error('something error');
+log.debug('something debug');
+log.notice('something notice');
+log.warning('something warning');
+log.alert('something alert');
+log.critical('something critical');
