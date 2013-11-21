e30ed8cf2d8f994aed48ea5f704dc60067b3b91c
johnymonster <jnelson11@gmail.com>
2013-11-21 15:17:59 -0500

    Adding loggly log bridge

---
+++ test/loggly.js
@@ -0,0 +1,18 @@
+/**
+ * This is more of a usage example than an actual unit test
+ *
+ * Setup your own development.yaml file in the config directory using the default structure
+ * adding your own auth_token and messages should be sent to loggly
+ *
+ * @type {exports}
+ */
+var CONF = require('config');
+
+process.env.NODE_LOGGER_LOGGLY_TOKEN = CONF.test.auth_token;
+process.env.NODE_LOGGER_LOGGLY_SUBDOMAIN = 'pmp';
+
+var log   = require('../')('loggly', 'debug');
+
+log.debug('test debug level');
+log.info('test info level');
+log.error('test error level');
